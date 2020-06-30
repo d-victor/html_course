e09a117b31ee97f9b024b978e22c572d2eb77c6a
katerina Nemerovetska <nemerovetskayaka@gmail.com>
2020-06-30 20:03:51 +0300

    js file was added

---
+++ katya/JS/alert.js
@@ -0,0 +1,36 @@
+alert( "I am JS");
+let 
+    name = "jon";
+    admin = name;
+    alert ( admin );
+
+    var ourContryRiver = "Dnipro";
+    var userName = 'Taras';
+    const yearOfBirth = 2000;
+    var numberYourHouse = 75;
+    var hasChildren = true;
+    var guest = null;
+    var userName = undefined;
+    typeof userName;
+    var user = {
+        firstName = "Roman",
+        lastName = "Korobchuk",
+        dataOfBirth = 07.09.1980,
+    }
+    var name = 'Tom';
+    var age = 20;
+   var customer = [ "name", age ];
+   var typeProduct = "shorts";
+   var quantity = 10;
+   var available = true;
+   var availableSize = andefined;
+   var product = [ "typeProduct", quantity, true, andefined];
+   var goods = [ {skirts},{shorts}];
+   var ListOfGoods = [[product],[goods]];
+
+
+
+
+
+
+    console.log(typeof userName);
