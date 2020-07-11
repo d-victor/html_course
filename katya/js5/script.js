7fe8d443bc7e3ac48b031fd08e9e228afe6fa48e
Viktor Dehtiarov <436750@gmail.com>
2020-07-11 22:40:08 +0300

    fix code

--- katya/js5/script.js
+++ katya/js5/script.js
@@ -1,31 +1,19 @@
-function nameLastName(name, lastName){
-
-    if( !name && !lastName ){
-        return 0;
-    }
-    console.log(`Your name ${name}.Your Lastname ${lastName}`);
-    return 1;
+function nameLastName(name, lastName) {
+    if (!name && !lastName) return false;
+    
+    console.log(`Your name ${name}. Your Lastname ${lastName}`);
+    return true;
 }
-function nl(){
-   var name = prompt('What is you name?');
-   var lastName = prompt('What is you LastName?');
-   return [name,lastName];
+
+function getName(question) {
+    return prompt(question);
 }
-//  var  list = nl();
-//  console.log(list);
-//  nameLastName(list[0], list[1]);
 
- function getName(question){
-     return prompt(question);
- }
-//  var status = ;
-//   console.log(typeof status, status);
-  getError(+nameLastName(getName('What is you name?'), getName('What is you LastName?')))
+getError(nameLastName(getName('What is you name?'), getName('What is you LastName?')), 'You answered incorrectly!');
 
-
- function getError(statusError, massege){
-     if(statusError === false){console.log(statusError);
+function getError(statusError, massege) {
+    if (!statusError) {
         massege = massege || 'Error';
         console.error(massege)
-     }
- }
+    }
+}
