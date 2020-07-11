81f5bf1ec5e8da6a3f74c9c3f0b44d68bd49fa35
katerina Nemerovetska <nemerovetskayaka@gmail.com>
2020-07-11 17:38:53 +0300

    homework has done

---
+++ katya/js/script.js
@@ -0,0 +1,169 @@
+// var i = 3; /// Виведе 3,2,1
+
+// while (i){
+//     console.log( i-- );
+// }
+// var a = 3;
+// do {
+//     console.log(a--);
+// } while (a);
+
+
+//  var c = 0;
+
+//  while (c < 5){
+//    console.log(c);
+    //   ++c;
+// }                     // 1,2,3,4
+
+//  while (c < 5){
+//     console.log(c);
+        // c++;
+// }                     // 1,2,3,4,5
+
+// do {
+//     console.log(c);
+        // ++c;
+// } while (c < 5);
+
+// do {
+//     console.log(c);
+        // c++;
+// } while (c < 5);
+
+// for (c = 0;c < 5;c++){
+//     console.log(c);
+// }
+// for (c = 0;c < 5;c++){
+//     console.log(c);
+// }
+
+// for (var f = 0; f < 5; f++) console.log(f); // Від 0 до 4
+
+// for (var f = 0; f < 5; ++f) console.log(f); // Від 0 до 4
+ 
+// var f = 0;
+
+// while(f < 5){
+//    console.log(f);
+//     ++f;
+// }
+// var xz = 0;
+// while(xz < 5){
+//     console.log(xz);
+        // xz++;
+// }
+
+// var ff = 0;
+
+//  do{
+//     console.log(ff);
+//      ff++;
+//  } while( ff < 5 );
+
+//  var fff = 0;
+
+//  do{
+//     console.log(fff);
+        // +fff;
+//  } while( fff < 5 );
+
+//  for( var dd = 2; dd <= 10;++dd){
+
+//     if (dd % 2 === 0){
+    // console.log(dd);
+    // }
+//     
+//  }
+  
+//  var as = 2;
+
+//  while ( as <= 10 ){
+//      if (as % 2 === 0){
+//     console.log(dd);
+//      ++dd;
+// }
+//      
+//  }
+// var da = 2;
+//  do {
+    // if (da % 2 == 0){
+//       console.log(++da)
+//     }
+// //     
+//  } while (da <= 10);
+
+// var k = 0;
+// while(k < 3){
+//     alert( `number ${k}!`);
+//     k++;
+// }
+//   var num;
+// do {
+//     num = prompt( 'Enter number', '' );
+// }while ( num <= 100 && num );
+
+// var numb = prompt( 'Enter number', '' );
+// while( numb < 100 ){
+//    numb = prompt( 'Enter number', '' ); 
+// }
+
+// var n = 10;
+// label: for (var p = 2; p<= n; p++){
+//     for (var s = 2; s < p; s++){
+//         if (p % s === 0) continue label;
+//     }
+//     console.log(p);
+// }
+        
+
+// var number = 1;
+
+// while( ++number < 100 ){
+//     alert(number);
+
+// }
+
+        // 1 //
+
+// var result = '';
+// var i = 0;
+// do {
+//    i += 1;
+//    result += i + ' ';
+// } while (i < 1000);
+// document.write(result);
+
+        /// 2 ///
+// var y = 1;
+// for (y = 1; y < 10;y++){
+//       if ( y % 2 ) {
+
+//         alert (y + ' Не парне');     
+//       } else {
+
+//         alert (y + ' Парне')    
+//       }
+// }
+
+ 
+        // 3 //
+    //   var ts = 5;
+
+    //  while( ts <= 10000 ){
+    //     console.log(ts ** 2); 
+    //  }
+
+       // 5 //
+
+//        for (var i = 1; i <= 4; i++) {
+
+//         for (var j = 1; j <= 4; j++) {
+      
+//           console.log(` ${i},${j}`);
+      
+//         }
+//       }
+      
+        
+      
