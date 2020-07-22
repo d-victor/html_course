67aa8a2bc44d3ebeb00522e3e2a677019392addf
Viktor Dehtiarov <436750@gmail.com>
2020-07-22 19:40:58 +0300

    fix code

--- katya/string/string.js
+++ katya/string/string.js
@@ -1,14 +1,8 @@
-var text;
-
-function getWord(text) {
-    console.log(text.indexOf('born',0));
-
+function getWord(text, word) {
+    return text.indexOf(word);
 }
 
-getWord('Gaston Leroux was French. He was born in Paris on 6th May 1868.Leroux was a big, heavy man. He had a large black beard.'
-);
-
-var str;
+console.log(getWord('Gaston Leroux was French. He was born in Paris on 6th May 1868.Leroux was a big, heavy man. He had a large black beard.', 'born'));
 
 function getSecond(str) {
     if (!str) return str;
@@ -17,120 +11,93 @@ function getSecond(str) {
 }
 console.log(getSecond(prompt('Writting something')));
 
-var str1;
-var updated;
 
-function truncate(str1, maxlenght) {
-
-    if (str1 > maxlenght) {
-        updated = maxlenght + "...";
-        return updated;
+function truncate(str1, maxLength) {
+    if (str1.length > maxLength.length) {
+        maxLength = maxLength + "...";
+        return maxLength;
     } else {
         return str1;
     }
-
 }
 
 console.log(truncate('That is what I would like to say on this topic', 'That is what I would like'));
 
-var str3;
-
 function currencyValue(str3) {
     return +str3.slice(1);
-
 }
 
 console.log(currencyValue('$100'));
 
 
-var str4;
-var count = -1;
-var position;
 
-function countPosition(str4) {
-
+function countPosition(str4, searchText) {
+    var count = -1;
+    var position;
+    
     while (position !== -1) {
-        position = str4.indexOf('e', position + 1);
+        position = str4.indexOf(searchText, position + 1);
         count++;
     }
-    console.log(count);
+    
+    return count;
 }
 
-countPosition('His reports were clear and exciting');
+console.log(countPosition('His reports were clear and exciting', 'e'));
 
-var str5;
-var newStr;
-
-function getString(str5) {
-    newStr = str5.slice(50);
+function getString(str5, position) {
+    var newStr;
+    
+    newStr = str5.slice(position);
+    
     for (let i = 0; i < newStr.length; i++) {
         if (i % 2) {
             console.log(newStr[i].toUpperCase());
         } else {
             console.log(newStr[i].toLowerCase());
         }
-
     }
-
 }
 
-getString('Sometimes, he was in trouble with his parents and with the city official.');
+getString('Sometimes, he was in trouble with his parents and with the city official.', 10);
 
-function getFullName1(fullName,search) {
-    if (search === null || search === "" ){
+function getFullName1(fullName, search) {
+    if (search === null || search === "" ) {
         return -1;
     }
     fullName = fullName ? fullName : "";
     fullName = fullName.trim();
 
     var count = 0;
-
-    for (let i=0; i < fullName.length;){
-
-        i = fullName.indexOf(search, i);
-        if (i === -1) {
-            break;
-        }else {
-            i = fullName +1;
+    var i = 0;
+    
+    while (true) {
+        i = fullName.indexOf(search, i) + 1;
+        if (i !== 0) {
             count++;
+        } else {
+            break;
         }
-
     }
     return count;
 }
-var fullName2 = getFullName1(prompt('What is your name'),prompt('What search?'));
+var fullName2 = getFullName1(prompt('What is your name'), prompt('What search?'));
 
 console.log(fullName2);
 
-var story;
-var pos = -1;
-var advebStr;
-function getWor(story) {
-
-    while (advebStr !== -1){
-        advebStr = story.indexOf('stories',advebStr +1);
-        pos++;
-    }
-    console.log(pos);
-}
-(getWor('He wrote adventure stories, romances, detective stories and horror stories.'));
-
-
-var clear = clear;
-function getStr6(textHis) {
-    clear = textHis.includes('clear');
-    if (clear === clear){
+function getStr6(textHis, clear) {
+    if (textHis.includes(clear)) {
         console.log(textHis);
     }
 }
-getStr6('His reports were clear and exciting');
 
+getStr6('His reports were clear and exciting', 'clear');
 
-var ler;
-var man;
-var heavy;
 
 function getSl(ler) {
+    var man;
+    var heavy;
+    
     man = ler.slice(18);
     heavy =ler.substr(13,5);
     console.log(heavy + " " + man);
