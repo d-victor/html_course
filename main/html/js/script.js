78db30131b17e53c54384b85a20ed09655215dc1
katerina Nemerovetska <nemerovetskayaka@gmail.com>
2020-07-23 11:31:39 +0300

    The files are rebasing

---
+++ main/html/js/script.js
@@ -0,0 +1,31 @@
+var num = -0 | 0 | -1 | 2 | 2.1 | NaN | Infinity | -Infinity; //number
+var string = "'dfg\"dfgs' dfgsdfgdfsgsdfg" | 'dfsgd\'sf gdsfgs' | `gsdfgdfsg ${num}`;
+var bool = false | true;
+var undef = undefined;
+var nu = null;
+var objectArray = ["dsfsdf", bool, go, false] || new Array('sdfsd', 'dsfds');
+var name = "Petay";
+var age = 25 + 20;
+var objectObject = {
+    user: name,
+    age: age,
+    skill: ['Html', "Js", {a:1}],
+    work: {
+    
+    },
+    go: function () {
+    
+    }
+};
+typeof
+console.log(objectObject);
+var objectFunc = function(){
+    alert('dsfdsfdf');
+};
+
+function go() {
+    return 2 + 2;
+}
+
+console.log(go());
+
