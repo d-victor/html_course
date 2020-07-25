165cba0d239af3a8987f98ccb1eec7a0e7a1dcb6
katerina Nemerovetska <nemerovetskayaka@gmail.com>
2020-07-25 16:21:58 +0300

    the homework has done

--- katya/homework_js/homeWork1.js
+++ katya/homework_js/homeWork1.js
@@ -1,78 +1,69 @@
-//
-function setFormatFirstLastName( FirstLastName, pattern) {
-    if (FirstLastName !== Number || countPosition(pattern,'a') !== Number){
+
+function setFormatFirstLastName(FirstLastName, pattern) {
+    if (FirstLastName == Number || countPosition(pattern, 'x') == FirstLastName.length) {
         return false;
     }
 
     var returnFirstLastName = '';
     var count = 0;
 
-    for (let i=0; i < pattern.length; i++){
-        if (pattern[i] !== 'a'){
+    for (let i = 0; i < pattern.length; i++) {
+        if (pattern[i] !== 'x') {
             returnFirstLastName += pattern[i];
-        }else {
-            returnFirstLastName += FirstLastName[count];
+        }else if (FirstLastName[count] === undefined){ continue;
+        } else {
+            returnFirstLastName += FirstLastName[count]; console.log(FirstLastName[count]);
             count++;
+
         }
     }
 
-    return FirstLastName;
+    return returnFirstLastName;
 }
 
-console.log(setFormatFirstLastName("DariaKorobchyk" , 'Aaaaa Aaaaaaaaa'));
+console.log(setFormatFirstLastName('Daria Korobchyk ', 'Mis xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'));
 
 
-/*function countPosition(str,searchStr) {
+function countPosition(str, searchStr) {
     var count = 0;
     var position;
+
 
     while (position !== -1) {
         position = str.indexOf(searchStr, position + 1);
 
-        if (position !== -1){
-            count++ ;
-        }
-        console.log(count);
-    }
-
-    return count;
-}*/
-
-var numberPhone = 380670001122;
-function setFormatNumber1( numberPhone, pattern) {
-    if (numberPhone.length !== 12 || countPosition(pattern,'x') !== 12){
-        return false;
-    }
-
-    var returnNumber = '';
-    var count = 0;
-
-    for (let i=0; i < pattern.length; i++){
-        if (pattern[i] !== 'x'){
-            returnNumber += pattern[i];
-        }else {
-            returnNumber += numberPhone[count];
+        if (position !== -1) {
             count++;
         }
-    }
 
-    return returnNumber;
-}
-
-console.log(setFormatNumber1( numberPhone, '+xx (xxx) xxx xx xx'));
-
-
-function countPosition(str,searchStr) {
-    var count = 0;
-    var position;
-
-    while (position !== -1) {
-        position = str.indexOf(searchStr, position + 1);
-
-        if (position !== -1){
-            count++ ;
-        }
     }
 
     return count;
 }
+
+//////////
+
+function searchValue(str, value){
+    var count = 0;
+    var position;
+    var str1;
+    var mainStr;
+
+
+    while (position !== -1) {
+        position = str.indexOf('blue', position + 1);
+        if (position !== -1) {
+            count++;
+        }
+        str1 = [str.slice(position, position + 4)]; console.log(str1);
+        mainStr = str1.push('blue');
+
+    }
+
+    return str1;
+    }
+
+console.log(searchValue('His trousers were of the lightest blue whipcord' +
+    ' his coat was blue; his waistcoat was blue velvet, with blue glass buttons;' +
+    ' and in the matter of blue tie.', 'blue'));
+
