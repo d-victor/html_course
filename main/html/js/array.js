2c998d0bc279080ebbede9dd2bd286ad8d480553
Viktor Dehtiarov <436750@gmail.com>
2020-08-01 17:26:31 +0300

    lesson 11. JS Array. Array method forEach, map, sort, revers, join, reduce

--- main/html/js/array.js
+++ main/html/js/array.js
@@ -3,7 +3,7 @@ var b = a;
 b= b+b+b+b;
 console.log(a, b);
 
-var arr = [1, '1', 0, 1, 0, 0, 0, 1, 1, '1', '2'];
+var arr = [1, 0, 1, 15, 20, 0, 2, 16];
 
 arr.push('ddd');
 arr.splice(arr.length - 2, 1);
@@ -45,3 +45,35 @@ console.log(arr.filter(function (elem, index, arr) {
     return elem === 1;
 }));
 
+console.log(arr);
+
+for (var i = 0; i < arr.length; i++){
+    console.log(arr[i], i, 'for');
+    if (i === 3) break;
+}
+
+for (var key in arr) {
+    console.log(arr[key], key);
+}
+
+arr.forEach(function (item, index) {
+    console.log(item)
+});
+
+console.log(arr.map(function (value, index, array) {
+    return value + 10;
+}), arr);
+
+console.log(arr.sort(function (a, b) {
+    return a - b;
+}), arr);
+
+console.log(arr.reverse());
+
+var d = 'a,b,c,d,e';
+
+console.log(d.split(',').reverse().join(','));
+
+console.log(arr.reduce(function (previousValue, item) {
+    return previousValue + item;
+}));
