12bc83dbc4a1f0c8f6c850bf582402b4481218b1
katerina Nemerovetska <nemerovetskayaka@gmail.com>
2020-08-03 17:41:36 +0300

    the task has done

--- katya/task1-js/task3.js
+++ katya/task1-js/task3.js
@@ -1,5 +1,5 @@
 var nameList = 'Inna, Daria, Masha, Ira, Olya';
-/*function changePossitionName(nameList, targetName, name2) {
+function changePossitionName(nameList, targetName, name2) {
     if(!nameList){
         return false;
     }
@@ -18,8 +18,8 @@ function changeElemArray(array, index1, index2) {
     array[index2] = temp;
 
     return array;
-}*/
-/*
+}
+
 function nameConsoleLog(nameList) {
     if(!nameList){
         return false;
@@ -40,50 +40,47 @@ function setPrefixName(nameList, prefix) {
           return prefix + elem
     })
     return nameList;
-}
+
+
 var finalPrefixNameList = setPrefixName(finalName, 'mis ');
-*/
 
-/*
+
+
 var finalPrefixName = arrayToString(finalPrefixNameList, ', ');
-*/
 
-/*
+
+
 console.log(finalPrefixNameList);
-*/
+
 
 function stringToArray(string, separator) {
     var arr = string.split(separator);
     return arr;
 }
 
-/*
+
 function arrayToString(arr,separator){
     var string = arr.join(separator);
     return string;
 }
 
-*/
 
 
 var nameCollectionList = changePosName(stringToArray(nameList,', '),[['Olya','Daria'],['Ira','Inna']])
 function changePosName(nameCollectionList, changeNameList) {
     console.log(nameCollectionList, changeNameList);
-    
-    var indexName1 = nameCollectionList.indexOf(changeNameList[0][0]);
-    var indexName2 = nameCollectionList.indexOf(changeNameList[0][1]);
-    var indexName3 = nameCollectionList.indexOf(changeNameList[1][0]);
-    var indexName4 = nameCollectionList.indexOf(changeNameList[1][1]);
 
-    
+    var indexName1 = findIndexName(nameCollectionList, changeNameList[0][0]);
+    var indexName2 = findIndexName(nameCollectionList, changeNameList[0][1]);
+    var indexName3 =  findIndexName(nameCollectionList, changeNameList[1][0]);
+    var indexName4 = findIndexName(nameCollectionList, changeNameList[1][1]);
     nameCollectionList = nameCollectionList.slice(0);
     var temp = nameCollectionList[indexName4];
     nameCollectionList[indexName4] = nameCollectionList[indexName1];
     nameCollectionList[indexName1] = temp;
-    
 
     return nameCollectionList;
-    
+
 }
 console.log(nameCollectionList);
 
@@ -92,5 +89,11 @@ function findIndexName(arr, possition) {
     return indexName;
  }
 
+// находить видаляє елемент
+function findDelItem(fruitsList,fruit) {
+    var delItem = fruitsList.splice(fruitsList.indexOf( fruit),1);
 
+    return fruitsList;
+}
+console.log(findDelItem(['Orange','Peach','Banana','Apple','Lemon'],'Lemon'))
 
