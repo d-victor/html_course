c0906c9ce058d5296e14b14fbaaeeb3f68647af5
katerina Nemerovetska <nemerovetskayaka@gmail.com>
2020-08-20 14:48:48 +0300

    the part of task has done

--- katya/homework_js/homework8.js
+++ katya/homework_js/homework8.js
@@ -2,6 +2,8 @@ function Cars(options) {
     this.color = options.color;
     this.model = options.model;
     this.year = options.year;
+    this.driverOwner = null;
+    this.driverList = [];
 }
 
 function Driver(options) {
@@ -10,62 +12,78 @@ function Driver(options) {
     this.drivingExperience = options.drivingExperience;
 }
 
-Cars.prototype.getSitName = getSitName;
-Cars.prototype.sayDriver = sayDriver;
+Cars.prototype.setDriver = setDriver;
+Cars.prototype.addOwnerCar = addOwnerCar;
+Cars.prototype.addDriverAccess = addDriverAccess;
 Cars.prototype.sayAboutCar = sayAboutCar;
 Cars.prototype.changeColor = changeColor;
-Cars.prototype.sitDriverOwner = sitDriverOwner;
-Cars.prototype.LetGoDrive = letGoDrive;
 Cars.prototype.sayAboutDrive = sayAboutDrive;
-Cars.prototype.stopGo = stopGo;
-Cars.prototype.setIntervalDrive = setIntervalDrive;
-Driver.prototype.sitCar = sitCar;
-Driver.prototype.getSitname = getSitName;
-Driver.prototype.sayDriver = sayDriver;
-Driver.prototype.sayAboutDrive = sayAboutDrive;
-Driver.prototype.stopGo = stopGo;
+Cars.prototype.stop = stop;
 
-var car1 = new Cars({
+Driver.prototype.sayDriver = sayDriver;
+
+var carVolvo = new Cars({
     model: 'Volvo',
     color: 'red',
     year: 2017,
 });
-var car2 = new Cars({
+var carNissan = new Cars({
     model: 'Nissan',
     color: 'white',
     year: 2018,
 });
-var car3 = new Cars({
+var carBMW = new Cars({
     model: 'BMW',
     color: 'black',
     year: 2019,
 });
 
-var driver1 = new Driver({
-    name: 'Olena',
+var driverElena = new Driver({
+    name: 'Elena',
     age: 30,
     drivingExperience: 10,
 });
-var driver2 = new Driver({
-    name: 'Iruna',
+var driverIrina = new Driver({
+    name: 'Irina',
     age: 20,
     drivingExperience: 1,
 });
 
-var driver3 = new Driver({
-    name: 'Mariya',
+var driverMarina = new Driver({
+    name: 'Marina',
     age: 34,
     drivingExperience: 10,
 });
-
-var driverOwner = [];
-
-function sitDriverOwner(driver) {
-    driverOwner.push(driver);
-    return driverOwner;
-}
-
-console.log(car1.sitDriverOwner(driver3), car1);
+var driverKarina = new Driver({
+    name: 'Karina',
+    age: 26,
+    drivingExperience: 4,
+});
+var driverNatalya = new Driver({
+    name: 'Natalya',
+    age: 24,
+    drivingExperience: 6,
+});
+var driverOleg = new Driver({
+    name: 'Oleg',
+    age: 31,
+    drivingExperience: 3,
+});
+var driverOleksandr = new Driver({
+    name: 'Oleksandr',
+    age: 25,
+    drivingExperience: 6,
+});
+var driverRoman = new Driver({
+    name: 'Roman',
+    age: 30,
+    drivingExperience: 10,
+});
+var driverVadim = new Driver({
+    name: 'Vadim',
+    age: 38,
+    drivingExperience: 1,
+});
 
 function sayAboutCar(driver) {
     return ' The model ' + this.model + ' is released ' + this.year + ' and it has color ' + this.color;
@@ -75,52 +93,58 @@ function changeColor(color) {
     this.color = color;
 }
 
-function sitCar(name) {
-    return this.name = name;
-}
-
-function getSitName() {
-    return name;
-}
-
 function sayDriver(driver) {
-    return 'The driver name is ' + this.name + ' she is ' + this.age + ' years old and has ' + this.drivingExperience + ' years driving experience.'
+    return 'The driver name is ' + this.name + ' my age is ' + this.age + ' and  I has ' + this.drivingExperience + ' years driving experience.'
 }
 
-function sayAboutDrive(driver, car) {
-    alert(driver.sayDriver() + ' Car is drive ' + car.sayAboutCar() + ' .');
+function sayAboutDrive(driver) {
+    return this.sayDriver() + ' car is drive... ' + this.sayAboutCar();
 }
 
-var go = sayAboutDrive.bind(car1, driver1, car2, driver2, car3, driver3);
+this.idInterval = setInterval(function (driver) {
+    this.sayAboutDrive(driver);
+}.bind(this),1000);
 
-function setIntervalDrive(go) {
-    var interval = setInterval(go, 1000);
-    var count = 1;
-
-    while (count < 3) {
-        if (count < 3) {
-            count++;
-            console.log(count)
-        } else if (count === 3) {
-            stopGo(interval);
-        }
-    }
-}
-
-function stopGo(interval) {
+function stop(interval) {
     clearInterval(interval);
 }
 
-function letGoDrive(driver, car) {
-    if (driverOwner.includes(driver)) {
-        return 1;
-    } else {
-        if (confirm('Sorry!We do not have such a driver. We have: ' + driver.sayDriver() + 'We put someone behind the wheel?')) {
-            car1.sitDriverOwner(driver);
-        }
+function setDriver(driver) {
+    console.log(driver, driver.name, this.driverOwner);
+    if(driver.name && !this.driverOwner){
+        this.driverOwner = driver;
     }
-    return setIntervalDrive(go);
+    return this.driverOwner;
 }
 
-console.log(letGoDrive(driver1, car1));
+function addOwnerCar(driver) {
+    this.driverList.push(driver);
+    return this.driverList;
+}
+
+carBMW.addOwnerCar(driverIrina);
+carBMW.addOwnerCar(driverMarina);
+carBMW.addOwnerCar(driverElena);
+carNissan.addOwnerCar(driverKarina);
+carNissan.addOwnerCar(driverOleg);
+carNissan.addOwnerCar(driverNatalya);
+carVolvo.addOwnerCar(driverVadim);
+carVolvo.addOwnerCar(driverRoman);
+carVolvo.addOwnerCar(driverOleksandr);
+
+
+function addDriverAccess(driver) {
+    if(this.driverList.includes(driver)){
+        this.setDriver(driver);
+    }else{
+        confirm('Access is denied! We change the owner? ');
+    }
+
+    return driver;
+}
+function removeOwner(driver){
+
+}
+
+console.log(carVolvo.addDriverAccess(driverOleg));
 
