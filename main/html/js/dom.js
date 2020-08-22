7664e22c38dbc98238cd37ac543b83301ffef035
Viktor Dehtiarov <436750@gmail.com>
2020-08-22 19:22:47 +0300

    lesson 15. JS DOM. Create Element. Get/Set Attribute. ClassList. Append... TextContent

--- main/html/js/dom.js
+++ main/html/js/dom.js
@@ -1,4 +1,4 @@
-
+/*
 console.log(document.getElementById('main'));
 console.log(document.querySelector('.header'));
 console.log(document.querySelectorAll('[href]'));
@@ -7,5 +7,17 @@ var linkList = document.querySelectorAll('[href]');
 linkList.forEach(function (link) {
     console.log(link);
     link.style.color = 'red';
-});
+});*/
 
+var elem = document.createElement('div');
+elem.setAttribute('type', 'text');
+elem.setAttribute('value', 'text');
+elem.classList.add('text', 'sdfds', 'dsfg', 'sdf');
+elem.classList.remove('text', 'sdf');
+elem.classList.contains('text');
+elem.classList.toggle('text');
+
+elem.textContent = 'aaaaaaaaaaaaaaaaaaaaaa dsfgfde';
+
+console.log(elem);
+document.body.append(elem);
