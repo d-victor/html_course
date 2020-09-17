5a61b280d1417daba0c0fcc3d04f492a6b5b9260
katerina Nemerovetska <nemerovetskayaka@gmail.com>
2020-09-17 21:49:17 +0300

    the part of main has stylized

--- katya/avtostore/script.js
+++ katya/avtostore/script.js
@@ -2,16 +2,16 @@
 (function () {
     var btn = document.querySelector('.subHeader_btn-mobile');
     var parent = document.querySelector('.header_row');
-    console.log(btn);
-    btn.addEventListener('click', function (event) {
-        if (event.target) {
+
+    parent.addEventListener('click', function (event) {
             parent.classList.add('open');
-        }
+        console.log('cl',parent)
     });
     btn.addEventListener('click', closeInput);
 
     function closeInput(event) {
-
+        parent.classList.remove('open');
     }
+
 
 })();
