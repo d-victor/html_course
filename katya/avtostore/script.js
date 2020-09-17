d5a773cfa0bd73ffad2210af3924eab4f0435a5c
katerina Nemerovetska <nemerovetskayaka@gmail.com>
2020-09-17 16:46:51 +0300

    search input was fixed

--- katya/avtostore/script.js
+++ katya/avtostore/script.js
@@ -1,6 +1,14 @@
-var btn = document.querySelector('.subHeader_btn-mobile');
-console.log(btn);
-btn.addEventListener('click', function (e) {
-    console.log('click');
-    btn.classList.add('open');
-});
+(function () {
+    var btn = document.querySelector('.subHeader_btn-mobile');
+    console.log(btn);
+    btn.addEventListener('click', function (e) {
+        console.log('click');
+        btn.classList.add('open');
+    });
+    btn.addEventListener('click', closeInput);
+
+    function closeInput(event) {
+       
+    }
+
+})();
