750a46a04632ac39aa35de90407e9597b38de38d
katerina Nemerovetska <nemerovetskayaka@gmail.com>
2020-09-17 16:49:28 +0300

    stash apply

--- katya/avtostore/script.js
+++ katya/avtostore/script.js
@@ -1,14 +1,17 @@
+
 (function () {
     var btn = document.querySelector('.subHeader_btn-mobile');
+    var parent = document.querySelector('.header_row');
     console.log(btn);
-    btn.addEventListener('click', function (e) {
-        console.log('click');
-        btn.classList.add('open');
+    btn.addEventListener('click', function (event) {
+        if (event.target) {
+            parent.classList.add('open');
+        }
     });
     btn.addEventListener('click', closeInput);
 
     function closeInput(event) {
-       
+
     }
 
 })();
