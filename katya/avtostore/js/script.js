9e55eb6715773bf6c5d592c913cc1a760f2878fb
katerina Nemerovetska <nemerovetskayaka@gmail.com>
2020-10-12 15:15:28 +0300

    the bag has fixed

--- katya/avtostore/js/script.js
+++ katya/avtostore/js/script.js
@@ -17,8 +17,13 @@
     function switchGridList(event) {
         if (event.currentTarget.dataset.list){
             parentGrid.classList.add('grid');
+            if (btnGrid.classList.contains('active')) btnGrid.classList.remove('active');
+            event.currentTarget.classList.add('active')
+
         }else if(event.currentTarget.dataset.grid) {
             parentGrid.classList.remove('grid');
+            if (btnList.classList.contains('active')) btnList.classList.remove('active');
+            event.currentTarget.classList.add('active');
         }
     }
     btnGrid.addEventListener('click', switchGridList);
@@ -28,20 +33,48 @@
 
 (function (){
     var tabLangList = document.querySelector('.tabLang_list');
+    var tabLangListLi = tabLangList.querySelectorAll('li');
     var tabLangTittle = document.querySelector('.tabLang_tittle');
 
-    tabLangList.addEventListener('click',function (e){
-        tabLangTittle.textContent = e.target.textContent;
+    tabLangListLi.forEach(function (li){
+        li.addEventListener('click',checkLang);
     });
+    function checkLang (e){
+        tabLangTittle.textContent = e.target.textContent;
+    }
+
 })();
 
 (function (){
   var currencyList = document.querySelector('.currency_list');
+  var currencyListLi = currencyList.querySelectorAll('li');
   var currencyTittle = document.querySelector('.currency_tittle');
 
-    currencyList.addEventListener('click',function (e){
+  currencyListLi.forEach(function (li){
+      li.addEventListener('click', checkCurrency)
+  });
+
+    function checkCurrency (e) {
         var contentCurrent = e.target.textContent;
         var contentCurrentList = contentCurrent.split('-');
         currencyTittle.textContent = contentCurrentList[0];
-    });
+    }
+
+})();
+
+(function (){
+    var priceElements = document.querySelectorAll('.price');
+    //var priceList = Array.prototype.slice.call(priceElem);
+    console.log(priceElements);
+
+   for (var i=0; i < priceElements.length; i++) {
+       var price = priceElements[i].textContent.substring(1);
+        price = parseFloat(price);
+
+       console.log(price);
+
+    }
+
+
+
 })();
