837ed6ce5e48bf75d6d3d3c3e1750ab01e8224df
katerina Nemerovetska <nemerovetskayaka@gmail.com>
2020-10-23 19:46:59 +0300

    bag fixed

--- katya/avtostore/js/script_2.js
+++ katya/avtostore/js/script_2.js
@@ -8,3 +8,19 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><i class="fas fa-angle-right"></i></button>',
     });
 });
+
+const sliderList = document.querySelectorAll('.slide');
+const mainImg = document.querySelector('.main-img');
+console.log(sliderList);
+    sliderList.forEach(function (elem) {
+        let selectedElem = false;
+        elem.addEventListener('click', function (e){
+            if (selectedElem === false) {
+                e.currentTarget.classList.add('active');
+                selectedElem = true;
+            } else {
+
+            }
+
+        });
+    });
