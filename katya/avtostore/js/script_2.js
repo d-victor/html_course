526415dcabc731e90e33006f8f806127382ccaca
katerina Nemerovetska <nemerovetskayaka@gmail.com>
2020-10-27 21:56:32 +0200

    main and footer had stylized

--- katya/avtostore/js/script_2.js
+++ katya/avtostore/js/script_2.js
@@ -9,30 +9,59 @@ $(document).ready(function () {
     });
 });
 
-
-const sliderList = document.querySelectorAll('.slide');
-const mainImg = document.querySelector('.main-img');
-
-sliderList.forEach(function (elem) {
-    elem.addEventListener('click', checkImg);
+$('.product-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-angle-left arrow-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="fas fa-angle-right arrow-right"></i></button>',
 });
 
-function checkImg(e) {
-    const checkElem = e.currentTarget;
-    mainImg.src = checkElem.src;
-    mainImg.classList.add('change');
-}
+(function (){
+    const sliderList = document.querySelectorAll('.slide');
+    const mainImg = document.querySelector('.main-img');
+
+    sliderList.forEach(function (elem) {
+        elem.addEventListener('click', checkImg);
+    });
+
+    function checkImg(e) {
+        const checkElem = e.currentTarget;
+        mainImg.src = checkElem.src;
+        mainImg.classList.add('change');
+    }
+})();
 
 
 
-const tittleList = document.querySelectorAll('.tittle-text');
-const content = document.querySelectorAll('.content');
-console.log(tittleList,content);
+(function (){
+    const titleList = document.querySelectorAll('.tab-header__item');
 
-tittleList.forEach(function (elem) {
-    elem.addEventListener('click', toggleContent);
-});
+    titleList.forEach(function (elem) {
+        elem.addEventListener('click', toggleContent);
+    });
 
-function toggleContent(e) {
-    console.log(e.target);
-}
+    function toggleContent(e) {
+        let id = e.currentTarget.getAttribute('data-tab'),
+            content = document.querySelector(`.tab-content__item[data-tab="${id}"]`),
+            activeTitle = document.querySelector('.tab-header__item.active'),
+            activeContent = document.querySelector('.tab-content__item.active');
+
+        activeTitle.classList.remove('active');
+        e.currentTarget.classList.add('active');
+        activeContent.classList.remove('active');
+        content.classList.add('active');
+    }
+
+    const btnZoomList = document.querySelectorAll('.btn-zoom');
+
+        btnZoomList.forEach(function (btn) {
+       btn.addEventListener('click', zoomCard);
+    });
+
+    function zoomCard(e){
+        let id = e.currentTarget.getAttribute('data-btn'),
+        card = document.querySelector(`.card-slider[data-card="${id}"]`);
+        card.classList.toggle('active');
+    }
+})();
